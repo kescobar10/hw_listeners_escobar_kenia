1f90e3b6f60b14c7d45975c5235e7b7555be9196
Kenia Escobar <keniarescobar@gmail.com>
2017-11-04 11:35:49 -0400

    add new article tag and set increment number var

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var clickme = document.getElementsByTagName('h1')[0];
+var i = 1;
 
 clickme.addEventListener('click', function () {
     var newParagraph = document.createElement('p');
-    var i = 0;
-    newParagraph.innerHTML = '<p>This is click number 1</p>' + i++;
-    document.getElementsByTagName('body')[0].appendChild(newParagraph);
+    newParagraph.innerHTML = '<p>This is click number</p>' + i++;
+    document.getElementsByTagName('article')[0].appendChild(newParagraph);
 });

be9c5eb355526cb0bdbb870771897becce65eb72
Kenia Escobar <keniarescobar@gmail.com>
2017-11-03 22:26:29 -0400

    modify text for new paragraph to be added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var clickme = document.getElementsByTagName('h1')[0];
 
 clickme.addEventListener('click', function () {
     var newParagraph = document.createElement('p');
-    newParagraph.innerHTML = '<p>Hello</p>';
+    newParagraph.innerHTML = '<p>This is click number 1</p>';
     document.getElementsByTagName('body')[0].appendChild(newParagraph);
 });

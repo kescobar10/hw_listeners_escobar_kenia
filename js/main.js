b5fb7824203cecaeb857481818e4d721a49607a7
Kenia Escobar <keniarescobar@gmail.com>
2017-11-04 11:47:08 -0400

    fix the increment line formatting

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ var i = 1;
 
 clickme.addEventListener('click', function () {
     var newParagraph = document.createElement('p');
-    newParagraph.innerHTML = '<p>This is click number</p>' + i++;
+    newParagraph.innerHTML = '<p>This is click number ' + i++ + '</p>';
     document.getElementsByTagName('article')[0].appendChild(newParagraph);
 });

1a789b17c6c8ab8a6d5d26fbdf17bfbb6a48318e
Kenia Escobar <keniarescobar@gmail.com>
2017-11-03 22:41:32 -0400

    add a new variable to set up number increments

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var clickme = document.getElementsByTagName('h1')[0];
 
 clickme.addEventListener('click', function () {
     var newParagraph = document.createElement('p');
-    newParagraph.innerHTML = '<p>This is click number 1</p>';
+    var i = 0;
+    newParagraph.innerHTML = '<p>This is click number 1</p>' + i++;
     document.getElementsByTagName('body')[0].appendChild(newParagraph);
 });
